5e1c45f2a7d5bcb0e2c01354bc29370368d12c8b
Lucas Parteka <tutoriaisbrasil39@gmail.com>
2025-06-14 09:54:48 -0300

    Retangulo Vermelho interativo adicionado

--- src/main/java/ticTacToe/component/Button.java
+++ src/main/java/ticTacToe/component/Button.java
@@ -1,11 +1,18 @@
 package ticTacToe.component;
 import ticTacToe.gui.Paintable;
+import ticTacToe.gui.util.MouseListenerAdapter;
+import ticTacToe.gui.util.MouseMotionAdapter;
 
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 public class Button implements Paintable {
     private Point position = null;
     private Dimension dimension = null;
+    private boolean mouseOver = false;
 
     public Button(){
         this.position = new Point(0, 0);
@@ -46,10 +53,63 @@ public class Button implements Paintable {
         return this.dimension.height;
     }
 
+    public boolean isOver(Point point) {
+
+        int xLeft = this.position.x;
+        int yTop = this.position.y;
+        int xRight = xLeft + this.width();
+        int yBottom = yTop + this.height();
+
+        return ((point.x > xLeft && point.x < xRight) &&
+                (point.y > yTop && point.y < yBottom));
+    }
+
+
+    public MouseListener mouseListener() {
+        return new MouseListenerAdapter() {
+
+            @Override
+            public void mouseClicked(MouseEvent me) {
+
+                if(!isOver(me.getPoint())) return;
+
+                System.out.println("Salve!");
+
+            }
+        };
+    }
+
+    public MouseMotionListener mouseMotionListener() {
+        return new MouseMotionAdapter() {
+
+            @Override
+            public void mouseMoved(MouseEvent me) {
+
+                if(!isOver(me.getPoint())){
+                    mouseOver = false;
+                    return;
+                }
+
+                    mouseOver = true;
+
+            }
+        };
+    }
+
+
     //--interface Paintable
     @Override
     public void paint(Graphics g) {
+
+        if (mouseOver){
+            Color color = g.getColor();
+            g.setColor(Color.RED);
+            g.drawRect(position.x + 2, position.y + 2,
+                    dimension.width - 4 , dimension.height - 4);
+            g.setColor(color);
+        }
+        else {
         g.drawRect(position.x, position.y,
-                dimension.width, dimension.height);
+                    dimension.width, dimension.height);}
     }
 }
---
+++ src/main/java/ticTacToe/gui/util/MouseMotionAdapter.java
@@ -0,0 +1,14 @@
+package ticTacToe.gui.util;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public abstract class MouseMotionAdapter implements MouseMotionListener {
+
+    @Override
+    public void mouseMoved(MouseEvent e) {}
+
+    @Override
+    public void mouseDragged(MouseEvent e) {}
+
+}
